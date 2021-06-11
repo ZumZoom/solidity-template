06bcd220e2fd65197c2edf0c1ee591d8d92b385b
Mikhail Melnik <by.zumzoom@gmail.com>
2021-06-11 11:00:35 +0300

    add natspec

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-
+/**
+ * @title Example contract
+ * @author ZumZoom
+ * @notice
+ *  Example contract without any meaningful functionality.
+ */
 contract Example {
+    /**
+     * @notice
+     *  Example event
+     * @param a Value to log
+     */
     event Log(uint256 a);
 
+    /**
+     * @notice
+     *  Example function that is present just to have any function at all.
+     * @param a Value that will be logged
+     */
     function func(uint256 a) public {
         emit Log(a);
     }
