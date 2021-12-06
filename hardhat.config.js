f1b76dba99d7a0be529f9cd44181f2f2b1f2930d
Mikhail Melnik <by.zumzoom@gmail.com>
2021-12-06 09:17:32 +0300

    update deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 runs: 1000000,
             },
         },
-        version: '0.8.9',
+        version: '0.8.10',
     },
     namedAccounts: {
         deployer: {
