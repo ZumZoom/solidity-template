e61f99fc58c46c870d358402c3f9448c67c1b5d8
Mikhail Melnik <by.zumzoom@gmail.com>
2022-06-06 11:23:52 +0400

    update deps and configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,22 +5,11 @@ require('hardhat-deploy');
 require('hardhat-gas-reporter');
 require('solidity-coverage');
 
-const networks = require('./hardhat.networks');
+const { networks, etherscan } = require('./hardhat.networks');
 
 module.exports = {
-    etherscan: {
-        apiKey: {
-            mainnet: process.env.MAINNET_ETHERSCAN_KEY,
-            bsc: process.env.BSC_ETHERSCAN_KEY,
-            kovan: process.env.KOVAN_ETHERSCAN_KEY,
-            polygon: process.env.POLYGON_ETHERSCAN_KEY,
-            optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_KEY,
-            arbitrumOne: process.env.ARBITRUM_ETHERSCAN_KEY,
-            avalanche: process.env.AVALANCHE_ETHERSCAN_KEY,
-            xdai: process.env.XDAI_ETHERSCAN_KEY,
-            opera: process.env.FTM_ETHERSCAN_KEY,
-        },
-    },
+    etherscan,
+    networks,
     solidity: {
         settings: {
             optimizer: {
@@ -28,12 +17,11 @@ module.exports = {
                 runs: 1000000,
             },
         },
-        version: '0.8.10',
+        version: '0.8.14',
     },
     namedAccounts: {
         deployer: {
             default: 0,
         },
     },
-    networks,
 };
