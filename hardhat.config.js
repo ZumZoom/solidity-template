9a7999983f77eff6f42fa74273c69129462a7c43
Anton Bukov <k06aaa@gmail.com>
2022-10-28 16:54:43 +0200

    Update deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 runs: 1000000,
             },
         },
-        version: '0.8.15',
+        version: '0.8.17',
     },
     namedAccounts: {
         deployer: {
